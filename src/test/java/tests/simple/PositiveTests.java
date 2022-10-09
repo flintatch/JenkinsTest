7cae27a34a49365d32b96691ed8753e068423678
gvlad <gvladovsky@gmail.com>
2022-10-09 15:44:00 +0300

    added simple tests

---
+++ src/test/java/tests/simple/PositiveTests.java
@@ -0,0 +1,28 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(true);
+    }
+
+}
