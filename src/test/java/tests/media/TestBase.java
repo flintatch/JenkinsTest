91a5bbccd597032c6bfd8ed8346d2c0e6aa05242
gvlad <gvladovsky@gmail.com>
2022-10-09 20:58:09 +0300

    added Attachments

--- src/test/java/tests/media/TestBase.java
+++ src/test/java/tests/media/TestBase.java
@@ -1,16 +1,29 @@
 package tests.media;
 
+import Assisst.Attach;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 
 }
