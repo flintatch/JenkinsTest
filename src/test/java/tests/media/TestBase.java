8296050a585ce1f0f8352fd91e1efb7cb571c4ee
gvlad <gvladovsky@gmail.com>
2022-10-09 20:04:08 +0300

    added Form Test and Selenoid Link

--- src/test/java/tests/media/TestBase.java
+++ src/test/java/tests/media/TestBase.java
@@ -10,6 +10,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
 }
